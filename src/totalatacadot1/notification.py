5fcd7e34e3fbb2aaacddcf2912279f6be8535b8b
holynacll <alexandrecurylima@gmail.com>
2026-03-14 15:48:57 -0300

    Update notification.py

--- src/totalatacadot1/notification.py
+++ src/totalatacadot1/notification.py
@@ -1,14 +1,18 @@
-from dataclasses import dataclass
-import requests
 import json
+from dataclasses import dataclass
+
+import requests
 from loguru import logger
 
 from totalatacadot1.config import settings
-from totalatacadot1.repository import create_notification_item, update_notification_item_sent
+from totalatacadot1.repository import (
+    create_notification_item,
+    update_notification_item_sent,
+)
 
 
 @dataclass
-class Notification():
+class Notification:
     ticket_code: str
     vl_total: float
     operation_type: str
@@ -32,7 +36,6 @@ class Notification():
             "message": self.message,
         }
 
-
     def notify_discount(self):
         """Envia uma notificação para o endpoint configurado."""
         # Converte para JSON
@@ -40,24 +43,21 @@ class Notification():
             notification_data = self.to_dict()
             json_data = json.dumps(notification_data)
             headers = {"Content-Type": "application/json"}
-            url = settings.url_notification + '/items'
+            url = settings.url_notification + "/items"
 
             # Envia a requisição POST
             response = requests.post(url, data=json_data, headers=headers, timeout=5)
             response.raise_for_status()  # Levanta um erro para status HTTP ruins (4xx ou 5xx)
 
-            logger.info(f"Notificação enviada com sucesso para {url}. Resposta: {response.status_code}")
+            logger.info(
+                f"Notificação enviada com sucesso para {url}. Resposta: {response.status_code}"
+            )
             update_notification_item_sent(self.ticket_code)
         except requests.exceptions.Timeout:
             logger.warning("Timeout ao enviar notificação.")
-            create_notification_item(self.to_dict())
         except requests.exceptions.ConnectionError:
             logger.warning("Não foi possível conectar ao servidor de notificação.")
-            create_notification_item(self.to_dict())
         except requests.exceptions.RequestException as e:
             logger.error(f"Erro ao enviar notificação: {e}")
-            create_notification_item(self.to_dict())
         except Exception as e:
             logger.error(f"Erro inesperado ao enviar notificação: {e}")
-            create_notification_item(self.to_dict())
-
